a93179f0c247de26fa91045fee8fa7fe09c02a74
Henry Burgess <henryjburg@gmail.com>
2021-07-03 09:53:09 +1000

    Check window vars loaded correctly
    Check that jsPsych and Gorilla are loaded correctly before continuing.

--- src/timeline.ts
+++ src/timeline.ts
@@ -34,45 +34,55 @@ timeline.push({
 // Initialise jsPsych and Gorilla (if required)
 if (config.target === 'gorilla') {
   // Wait for the entire page to be loaded before initialising
-  // jsPsych
+  // jsPsych and Gorilla
   window.onload = () => {
     // Once all modules are loaded into the window,
     // access Gorilla API and jsPsych library
     const _gorilla = window['gorilla'];
     const _jsPsych = window['jsPsych'];
 
-    // Require any jsPsych plugins, so that they are
-    // loaded here
-    require('jspsych/plugins/jspsych-instructions');
-    _gorilla.ready(function() {
-      _jsPsych.init({
-        display_element: $('#gorilla')[0],
-        timeline: timeline,
-        on_data_update: function(data) {
-          _gorilla.metric(data);
-        },
-        on_finish: function() {
-          _gorilla.finish();
-        },
-        show_progress_bar: true,
-        show_preload_progress_bar: true,
+    // Make sure Gorilla and jsPsych are loaded
+    if (_gorilla && _jsPsych) {
+      // Require any jsPsych plugins, so that they are
+      // loaded here
+      require('jspsych/plugins/jspsych-instructions');
+      _gorilla.ready(function() {
+        _jsPsych.init({
+          display_element: $('#gorilla')[0],
+          timeline: timeline,
+          on_data_update: function(data) {
+            _gorilla.metric(data);
+          },
+          on_finish: function() {
+            _gorilla.finish();
+          },
+          show_progress_bar: true,
+          show_preload_progress_bar: true,
+        });
       });
-    });
+    } else {
+      console.error(`Fatal: Gorilla or jsPsych not loaded.`);
+    }
   };
 } else {
   // Once all modules are loaded into the window,
   // access jsPsych library
   const _jsPsych = window['jsPsych'];
 
-  // Require any jsPsych plugins, so that they are
-  // loaded here
-  require('jspsych/plugins/jspsych-instructions');
-  _jsPsych.init({
-    timeline: timeline,
-    on_finish: function() {
-      _jsPsych.data.get().localSave(`csv`, `intentions_${Date.now()}.csv`);
-    },
-    show_progress_bar: true,
-    show_preload_progress_bar: true,
-  });
+  // Make sure jsPsych is loaded
+  if (_jsPsych) {
+    // Require any jsPsych plugins, so that they are
+    // loaded here
+    require('jspsych/plugins/jspsych-instructions');
+    _jsPsych.init({
+      timeline: timeline,
+      on_finish: function() {
+        _jsPsych.data.get().localSave(`csv`, `intentions_${Date.now()}.csv`);
+      },
+      show_progress_bar: true,
+      show_preload_progress_bar: true,
+    });
+  } else {
+    console.error(`Fatal: jsPsych not loaded.`);
+  }
 }
