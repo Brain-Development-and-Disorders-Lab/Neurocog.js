11b0b95f62f029cd63eb2702b690bf196a376685
Henry Burgess <henryjburg@gmail.com>
2022-05-05 15:56:18 -0500

    Remove hanging timeout test

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,8 +1,4 @@
-import { clear, clearTimeouts } from "../src/lib/functions";
-
-// Configure Jest spy functionality
-jest.useFakeTimers();
-jest.spyOn(global, "clearTimeout");
+import { clear } from "../src/lib/functions";
 
 // Test 'clear' function
 describe("clearing page contents", () => {
@@ -17,21 +13,5 @@ describe("clearing page contents", () => {
     clear(dummyContent, false);
 
     expect(dummyContent.firstChild).toBeNull();
-  });
-});
-
-// Test 'clearTimeouts'
-describe("clearing timeouts", () => {
-  // Note: when using fake timeouts, by default there are two
-  // pre-existing timeouts. This is accounted for by expecting
-  // two timers to have been set already.
-  it("clears all timeouts", () => {
-    clearTimeouts();
-    expect(clearTimeout).toHaveBeenCalledTimes(2 + 0);
-  });
-
-  it("clears a range of timeouts", () => {
-    clearTimeouts([1, 2, 3]);
-    expect(clearTimeout).toHaveBeenCalledTimes(2 + 3);
   });
 });
